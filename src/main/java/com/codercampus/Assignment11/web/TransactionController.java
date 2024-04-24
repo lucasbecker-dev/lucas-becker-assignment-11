271ba0c6d77351057e75cad7b632ef109cf0ce82
Lucas Becker <lucasbecker.dev@gmail.com>
2024-04-24 17:04:10 -0400

    autowire in transactionService

--- src/main/java/com/codercampus/Assignment11/web/TransactionController.java
+++ src/main/java/com/codercampus/Assignment11/web/TransactionController.java
@@ -1,10 +1,19 @@
 package com.codercampus.Assignment11.web;
 
+import com.codercampus.Assignment11.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TransactionController {
+    private final TransactionService transactionService;
+
+    @Autowired
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
     @GetMapping("/transactions")
     public String getTransactions() {
         return "transactions";
